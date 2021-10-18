61c3fc1f190be56d041a5b0c9c70a55c9fb42de1
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-10-18 11:50:29 +0600

    add banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import banner from "../../../banner-img.png";
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <Container fluid="lg">
+            <Row className="h-100 d-flex align-items-center">
+                <Col  lg="5" md={6} xs={12}>
+                    <h1 className="banner-title"><span className="title">Dental</span> Care</h1>
+                   <p>Dental care is the maintenance of healthy teeth and may refer to: Oral hygiene, the practice of keeping the mouth and teeth clean in order to prevent dental disorders</p>
+                   <button className="btn btn-lg" style={{background: "#23CB77"}}>Book A Viste</button>
+                </Col>
+                <Col  lg="7" md={6} xs={12}>
+                <Image className="banner-img" style={{height:"80vh"}} src={banner}  fluid />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/TopNavbar/TopNavbar.js
+++ src/Pages/Shared/TopNavbar/TopNavbar.js
@@ -3,24 +3,34 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 const TopNavbar = () => {
     return (
         <div>
-             <Navbar bg="light" expand="lg">
+            <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home"><h3><span className="title">Dental</span>Care</h3></Navbar.Brand>
-                  
-                        <Nav className="ms-auto">
-                            <Nav.Link>
-                                <i className="fas fa-envelope me-1"></i>
-                                info@example.com
-                            </Nav.Link>
-                            <Nav.Link>
-                            <i className="fas fa-phone me-1"></i> + 01 994 589 676
-                            </Nav.Link>
-                            <Nav.Link>
-                            <i className="fas fa-home me-1"></i>Kurigram, RK 10012, BD
-                            </Nav.Link>
+                    <Navbar.Brand href="#home">
+                        <h3 className="d-flex align-items-center">
+                            <img
+                                alt=""
+                                src="./favicon.ico"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                            <span className="title">Dental</span>Care</h3>
+                    </Navbar.Brand>
 
-                        </Nav>
-                  
+                    <Nav className="ms-auto">
+                        <Nav.Link>
+                            <i className="fas fa-envelope me-1"></i>
+                            info@example.com
+                        </Nav.Link>
+                        <Nav.Link>
+                            <i className="fas fa-phone me-1"></i> + 01 994 589 676
+                        </Nav.Link>
+                        <Nav.Link>
+                            <i className="fas fa-home me-1"></i>Kurigram, RK 10012, BD
+                        </Nav.Link>
+
+                    </Nav>
+
                 </Container>
             </Navbar>
         </div>
